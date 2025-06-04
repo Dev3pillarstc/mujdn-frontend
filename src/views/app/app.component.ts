351ab9e58d0035dd70171099dc90cf638540ee38
Mahmoud Soliman <mahmoud.soliman@3pillars-eg.com>
2025-06-04 10:34:15 +0300

    finish translation without refactor

--- src/views/app/app.component.ts
+++ src/views/app/app.component.ts
@@ -1,12 +1,13 @@
 import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {TranslateService} from '@ngx-translate/core';
 import {LocalStorageService} from '@/services/shared/local-storage.service';
 import {LOCALSTORAGE_ENUM} from '@/enums/local-storage-enum';
 import {LANGUAGE_ENUM} from '@/enums/language-enum';
 import {LAYOUT_DIRECTION_ENUM} from '@/enums/layout-direction-enum';
-import {RouterOutlet} from '@angular/router';
 import {SpinnerComponent} from '../shared/spinner/spinner.component';
 import {CommonModule} from '@angular/common';
+import {LanguageService} from '@/services/shared/language.service';
 
 @Component({
   selector: 'app-root',
@@ -15,17 +16,17 @@ import {CommonModule} from '@angular/common';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  private translate = inject(TranslateService);
+  private translateService = inject(TranslateService);
+  private languageService = inject(LanguageService);
   private localStorageService = inject(LocalStorageService);
   declare direction: string;
 
   ngOnInit() {
-    this.translate.addLangs([LANGUAGE_ENUM.ENGLISH, LANGUAGE_ENUM.ARABIC]);
-    const currentLanguage = this.localStorageService.get(LOCALSTORAGE_ENUM.LANGUAGE);
-    currentLanguage && this.translate.use(currentLanguage);
-    !currentLanguage && this.translate.use(LANGUAGE_ENUM.ENGLISH) && this.localStorageService.set(LOCALSTORAGE_ENUM.LANGUAGE, LANGUAGE_ENUM.ENGLISH);
+    this.translateService.addLangs([LANGUAGE_ENUM.ENGLISH, LANGUAGE_ENUM.ARABIC]);
+    const currentSavedLanguage = this.localStorageService.get(LOCALSTORAGE_ENUM.LANGUAGE);
+    this.languageService.setLanguage(currentSavedLanguage);
 
-    this.translate.onLangChange.subscribe(event => {
+    this.translateService.onLangChange.subscribe(event => {
       event.lang == LANGUAGE_ENUM.ARABIC && (this.direction = LAYOUT_DIRECTION_ENUM.RTL) || (this.direction = LAYOUT_DIRECTION_ENUM.LTR);
     });
   }
--- src/views/layout/core-layout/header/header.component.ts
+++ src/views/layout/core-layout/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {LANGUAGE_ENUM} from '@/enums/language-enum';
+import {LANGUAGE_BUTTON_LABEL_ENUM} from '@/enums/language-button-label-enum';
+import {LocalStorageService} from '@/services/shared/local-storage.service';
+import {LOCALSTORAGE_ENUM} from '@/enums/local-storage-enum';
+import {LanguageService} from '@/services/shared/language.service';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +12,29 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  languageService = inject(LanguageService);
+  translateService = inject(TranslateService);
+  localStorageService = inject(LocalStorageService);
+  declare currentLanguage: string;
+  languageEnum = LANGUAGE_ENUM;
 
+  ngOnInit() {
+
+  }
+
+  getLanguageButtonText() {
+    let lang: string = LANGUAGE_BUTTON_LABEL_ENUM.ENGLISH;
+    if(this.translateService.currentLang === LANGUAGE_ENUM.ARABIC) {
+      lang = LANGUAGE_BUTTON_LABEL_ENUM.ENGLISH;
+    } else {
+      lang = LANGUAGE_BUTTON_LABEL_ENUM.ARABIC;
+    }
+    return lang;
+  }
+
+  changeLanguage() {
+    const targetLanguage = this.translateService.currentLang == LANGUAGE_ENUM.ENGLISH ? LANGUAGE_ENUM.ARABIC : LANGUAGE_ENUM.ENGLISH;
+    this.languageService.setLanguage(targetLanguage);
+  }
 }

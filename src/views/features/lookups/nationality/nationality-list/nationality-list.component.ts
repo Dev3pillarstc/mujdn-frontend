af3e0229d2c0a786d7771e205e7bfe344c162902
kareem-shehab <Kareem.Shehab@3pillarstc.com>
2025-06-10 15:02:05 +0300

    edit attendance logs and create nationality list page

---
+++ src/views/features/lookups/nationality/nationality-list/nationality-list.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Breadcrumb } from 'primeng/breadcrumb';
+import { FormsModule } from '@angular/forms';
+import { Select } from 'primeng/select';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FluidModule } from 'primeng/fluid';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { PaginatorModule, PaginatorState } from 'primeng/paginator';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-nationality-list',
+  imports: [
+    Breadcrumb,
+    FormsModule,
+    DatePickerModule,
+    FluidModule,
+    TableModule,
+    CommonModule,
+    RouterModule,
+    CommonModule,
+    PaginatorModule,
+    InputTextModule,
+  ],
+  templateUrl: './nationality-list.component.html',
+  styleUrl: './nationality-list.component.scss',
+})
+export default class NationalityListComponent {
+  items: MenuItem[] | undefined;
+
+  home: MenuItem | undefined;
+
+  date2: Date | undefined;
+  nationalities!: any[];
+  first: number = 0;
+  rows: number = 10;
+
+  ngOnInit() {
+    this.items = [
+      { label: 'لوحة المعلومات' },
+      { label: 'حركات حضور و انصراف الموظفين' },
+    ];
+    // Updated dummy data to match your Arabic table structure
+    this.nationalities = [
+      {
+        nationality: 'اسم الجنسية',
+      },
+      {
+        nationality: 'اسم الجنسية',
+      },
+    ];
+  }
+  onPageChange(event: PaginatorState) {
+    this.first = event.first ?? 0;
+    this.rows = event.rows ?? 10;
+  }
+}

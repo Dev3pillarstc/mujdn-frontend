d1c1fde0cc08203dbc1a471bad6039d6ff025562
kareem-shehab <Kareem.Shehab@3pillarstc.com>
2025-06-11 16:36:49 +0300

    add adding pupup

--- src/views/features/attendance-log/attendance-log-popup/attendance-log-popup.component.ts
+++ src/views/features/attendance-log/attendance-log-popup/attendance-log-popup.component.ts
@@ -1,12 +1,26 @@
 import { BasePopupComponent } from '@/abstracts/base-components/base-popup/base-popup.component';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DatePickerModule } from 'primeng/datepicker';
+import { Select } from 'primeng/select';
 
+interface Adminstration {
+  type: string;
+}
 @Component({
   selector: 'app-attendance-log-popup',
-  imports: [],
+  imports: [Select, DatePickerModule, FormsModule],
   templateUrl: './attendance-log-popup.component.html',
-  styleUrl: './attendance-log-popup.component.scss'
+  styleUrl: './attendance-log-popup.component.scss',
 })
 export class AttendanceLogPopupComponent extends BasePopupComponent {
+  adminstrations: Adminstration[] | undefined;
 
+  date2: Date | undefined;
+
+  selectedAdminstration: Adminstration | undefined;
+
+  ngOnInit() {
+    this.adminstrations = [{ type: 'عام' }, { type: 'خاص' }];
+  }
 }

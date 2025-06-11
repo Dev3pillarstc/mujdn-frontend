dfdfe8a820f7943578bbf34fa3cc064ba0c25ad4
Mahmoud Soliman <mahmoud.soliman@3pillars-eg.com>
2025-06-11 12:19:50 +0300

    refactor

--- src/views/features/attendance-log/attendance-log-popup/attendance-log-popup.component.ts
+++ src/views/features/attendance-log/attendance-log-popup/attendance-log-popup.component.ts
@@ -1,12 +1,10 @@
-import {Component} from '@angular/core';
-import {BasePopupComponent} from '@/components/base-popup/base-popup.component';
+import { BasePopupComponent } from '@/abstracts/base-components/base-popup/base-popup.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-attendance-log-popup',
   imports: [],
   templateUrl: './attendance-log-popup.component.html',
-  styleUrl: './attendance-log-popup.component.scss'
+  styleUrl: './attendance-log-popup.component.scss',
 })
-export class AttendanceLogPopupComponent extends BasePopupComponent {
-
-}
+export class AttendanceLogPopupComponent extends BasePopupComponent {}

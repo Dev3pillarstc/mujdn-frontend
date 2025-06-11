64ea967afd238037912110d29c99d1e85bd47419
kareem-shehab <Kareem.Shehab@3pillarstc.com>
2025-06-11 14:18:30 +0300

    add nationality popup and edit on popup style

--- src/views/features/lookups/nationality/nationality-list/nationality-list.component.ts
+++ src/views/features/lookups/nationality/nationality-list/nationality-list.component.ts
@@ -4,19 +4,26 @@ import { Breadcrumb } from 'primeng/breadcrumb';
 import { TableModule } from 'primeng/table';
 import { PaginatorModule, PaginatorState } from 'primeng/paginator';
 import { InputTextModule } from 'primeng/inputtext';
+import { BaseListComponent } from '@/abstracts/base-components/base-list/base-list.component';
+import { NationalityPopupComponent } from '../nationality-popup/nationality-popup.component';
 
 @Component({
   selector: 'app-nationality-list',
-  imports: [
-    Breadcrumb,
-    TableModule,
-    PaginatorModule,
-    InputTextModule,
-  ],
+  imports: [Breadcrumb, TableModule, PaginatorModule, InputTextModule],
   templateUrl: './nationality-list.component.html',
   styleUrl: './nationality-list.component.scss',
 })
-export default class NationalityListComponent implements OnInit {
+export default class NationalityListComponent
+  extends BaseListComponent<NationalityPopupComponent>
+  implements OnInit
+{
+  override dialogSize = {
+    width: '100%',
+    maxWidth: '600px',
+  };
+  override openDialog(): void {
+    this.openBaseDialog(NationalityPopupComponent as any);
+  }
   items: MenuItem[] | undefined;
 
   home: MenuItem | undefined;
--- src/views/features/lookups/nationality/nationality-popup/nationality-popup.component.ts
+++ src/views/features/lookups/nationality/nationality-popup/nationality-popup.component.ts
@@ -1,11 +1,11 @@
+import { BasePopupComponent } from '@/abstracts/base-components/base-popup/base-popup.component';
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-nationality-popup',
-  imports: [],
+  imports: [InputTextModule],
   templateUrl: './nationality-popup.component.html',
-  styleUrl: './nationality-popup.component.scss'
+  styleUrl: './nationality-popup.component.scss',
 })
-export class NationalityPopupComponent {
-
-}
+export class NationalityPopupComponent extends BasePopupComponent {}

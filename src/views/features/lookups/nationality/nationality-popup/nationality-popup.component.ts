59c3560c74188aab441d7a0cdc28250e9caaaa0f
kareem-shehab <Kareem.Shehab@3pillarstc.com>
2025-06-12 15:32:29 +0300

    edit on natinality page and permission page

--- src/views/features/lookups/nationality/nationality-popup/nationality-popup.component.ts
+++ src/views/features/lookups/nationality/nationality-popup/nationality-popup.component.ts
@@ -1,7 +1,7 @@
 import { BasePopupComponent } from '@/abstracts/base-components/base-popup/base-popup.component';
 import { Component, inject, OnInit } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nationality-popup',
@@ -9,18 +9,4 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './nationality-popup.component.html',
   styleUrl: './nationality-popup.component.scss',
 })
-export class NationalityPopupComponent extends BasePopupComponent implements OnInit {
-  fb = inject(FormBuilder);
-  declare form: FormGroup;
-
-  ngOnInit() {
-    this.createForm();
-  }
-
-  createForm() {
-    this.form = this.fb.group({
-      nameAr: ['', Validators.required],
-      nameEn: ['', Validators.required],
-    });
-  }
-}
+export class NationalityPopupComponent extends BasePopupComponent {}

820e0817f678dccbb20a164944616260e2fb85e4
Ali Ibrahim <49904905+aliibrahim14@users.noreply.github.com>
2025-06-18 15:55:06 +0300

    Finish Department

---
+++ src/views/features/department/department-header/department-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-department-header',
+  imports: [],
+  templateUrl: './department-header.component.html',
+  styleUrl: './department-header.component.scss'
+})
+export class DepartmentHeaderComponent {
+
+}

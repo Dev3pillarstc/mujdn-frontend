cbb9fdd7bbbec1b7e98b3e9aa582cf7343cfb76c
Mohamed Hosam <mohamed.hosam@3pillars-eg.com>
2025-06-22 15:16:53 +0300

    Removing my generic improvments

--- src/abstracts/base-crud-cast-service.ts
+++
@@ -1,9 +0,0 @@
-// abstracts/base-crud-cast-service.ts
-import { CastResponseCrud } from './cast-response-crud.decorator';
-import { BaseCrudService } from './base-crud-service';
-
-export function CastedCrudService<T>(model: () => new () => T) {
-  @CastResponseCrud<T>(model)
-  abstract class BaseCastedService extends BaseCrudService<T, any> {}
-  return BaseCastedService;
-}

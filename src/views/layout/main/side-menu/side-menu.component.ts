c48eb11f3b2f8bdef993e72ad167719125225cd1
Mahmoud Soliman <mahmoud.soliman@3pillars-eg.com>
2025-06-22 19:10:48 +0300

    return sidebar menu depend on role

--- src/views/layout/main/side-menu/side-menu.component.ts
+++ src/views/layout/main/side-menu/side-menu.component.ts
@@ -1,10 +1,11 @@
-import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { PanelMenuModule } from 'primeng/panelmenu';
-import { MenuItem } from 'primeng/api';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '@/services/auth/auth.service';
+import { SideBarLinksService } from '@/services/shared/side-bar-links.service';
+import { MenuItem } from '@/models/shared/menu-item';
 
 @Component({
   selector: 'app-side-menu',
@@ -12,81 +13,14 @@ import { AuthService } from '@/services/auth/auth.service';
   imports: [SidebarModule, PanelMenuModule, RouterModule, CommonModule],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {
+export class SideMenuComponent implements OnInit {
   @ViewChild('sidebarContainer', { static: true }) sidebarContainer!: ElementRef;
   sidebarVisible = false;
   isMobile = window.innerWidth <= 768;
   openedSubmenus = new Set<MenuItem>();
   authService = inject(AuthService);
-
-  menuItems = [
-    {
-      label: 'لوحة المعلومات',
-      iconUrl: 'assets/icons/menu-icons/home.svg',
-      routerLink: ['/dashboard'],
-    },
-    {
-      label: 'تقرير الحضور و الانصراف',
-      iconUrl: 'assets/icons/menu-icons/icon-stroke-rounded.svg',
-      routerLink: ['/dashboard'],
-    },
-    {
-      label: 'حركات الحضور و الانصراف',
-      iconUrl: 'assets/icons/menu-icons/icon.svg',
-      routerLink: ['/dashboard'],
-    },
-    {
-      label: 'الادارات',
-      iconUrl: 'assets/icons/menu-icons/tools.svg',
-      routerLink: ['/dashboard'],
-    },
-    {
-      label: 'قائمة الموظفين',
-      iconUrl: 'assets/icons/menu-icons/employees.svg',
-      routerLink: ['/dashboard'],
-    },
-    {
-      label: 'مسائلات توثيق التواجد',
-      iconUrl: 'assets/icons/menu-icons/icon2.svg',
-      routerLink: ['/dashboard'],
-    },
-    {
-      label: 'الاستئذانات',
-      iconUrl: 'assets/icons/menu-icons/permissions.svg',
-      routerLink: ['/dashboard'],
-    },
-    {
-      label: 'الموظفون المُكلفون',
-      iconUrl: 'assets/icons/menu-icons/icon3.svg',
-      routerLink: ['/dashboard'],
-    },
-    {
-      label: 'قائمة الاجازات و الأعياد',
-      iconUrl: 'assets/icons/menu-icons/vacations.svg',
-      routerLink: ['/dashboard'],
-    },
-    {
-      label: 'الإعدادات',
-      iconUrl: 'assets/icons/menu-icons/settings.svg',
-      children: [
-        {
-          label: 'اعدادات الأذونات',
-          // iconUrl: 'assets/icons/permission.svg',
-          routerLink: ['/settings/permissions'],
-        },
-        {
-          label: 'اعدادات الاشعارات',
-          // iconUrl: 'assets/icons/notification.svg',
-          routerLink: ['/settings/notifications'],
-        },
-        {
-          label: 'قائمة الجنسيات',
-          // iconUrl: 'assets/icons/nationality.svg',
-          routerLink: ['/nationalities'],
-        },
-      ],
-    },
-  ];
+  sidebarLinksService = inject(SideBarLinksService);
+  menuItems: MenuItem[] = [];
 
   constructor() {
     window.addEventListener('resize', () => {
@@ -99,6 +33,12 @@ export class SideMenuComponent {
     if (!this.isMobile) {
       this.sidebarVisible = true;
     }
+  }
+
+  ngOnInit() {
+    this.authService.getUser().subscribe((_) => {
+      this.menuItems = this.sidebarLinksService.getSidebarLinks();
+    });
   }
 
   toggleSubmenu(item: MenuItem) {

b85846453a774656d6255195d0e881230aa82947
Mahmoud Soliman <mahmoud.soliman@3pillars-eg.com>
2025-06-24 15:44:59 +0300

    make Factory service and example on city

--- src/models/features/lookups/city/city.ts
+++ src/models/features/lookups/city/city.ts
@@ -4,6 +4,9 @@ import { CityService } from '@/services/features/lookups/city.service';
 import { CustomValidators } from '@/validators/custom-validators';
 import { Validators } from '@angular/forms';
 import { InterceptModel } from 'cast-response';
+import { LanguageService } from '@/services/shared/language.service';
+import { FactoryService } from '@/services/factory-service';
+import { LANGUAGE_ENUM } from '@/enums/language-enum';
 
 const { send, receive } = new CityInterceptor();
 
@@ -13,6 +16,19 @@ export class City extends BaseCrudModel<City, CityService> {
   declare nameAr: string;
   declare nameEn: string;
   declare fkRegionId: number;
+  private languageService!: LanguageService;
+
+  constructor() {
+    super();
+    this.languageService = FactoryService.getService('LanguageService');
+  }
+
+  getName(): string {
+    return this.languageService.getCurrentLanguage() == LANGUAGE_ENUM.ENGLISH
+      ? this.nameEn
+      : this.nameAr;
+  }
+
   buildForm() {
     const { nameAr, nameEn, fkRegionId } = this;
     return {
--- src/models/features/lookups/region/region.ts
+++ src/models/features/lookups/region/region.ts
@@ -4,6 +4,9 @@ import { RegionService } from '@/services/features/lookups/region.service';
 import { CustomValidators } from '@/validators/custom-validators';
 import { Validators } from '@angular/forms';
 import { InterceptModel } from 'cast-response';
+import { LANGUAGE_ENUM } from '@/enums/language-enum';
+import { FactoryService } from '@/services/factory-service';
+import { LanguageService } from '@/services/shared/language.service';
 
 const { send, receive } = new RegionInterceptor();
 
@@ -12,6 +15,19 @@ export class Region extends BaseCrudModel<Region, RegionService> {
   override $$__service_name__$$: string = 'RegionService';
   declare nameAr: string;
   declare nameEn: string;
+  private languageService!: LanguageService;
+
+  constructor() {
+    super();
+    this.languageService = FactoryService.getService('LanguageService');
+  }
+
+  getName(): string {
+    return this.languageService.getCurrentLanguage() == LANGUAGE_ENUM.ENGLISH
+      ? this.nameEn
+      : this.nameAr;
+  }
+
   buildForm() {
     const { nameAr, nameEn } = this;
     return {

7bc19ddce5a98efe996dcb89a0038d0305f3d8f1
Youssef <youssef.hassan@3pillars-eg.com>
2025-07-08 15:16:35 +0300

    Formatting

--- src/models/features/lookups/base-lookup-model.ts
+++ src/models/features/lookups/base-lookup-model.ts
@@ -1,20 +1,5 @@
-import { LANGUAGE_ENUM } from '@/enums/language-enum';
-import { FactoryService } from '@/services/factory-service';
-import { LanguageService } from '@/services/shared/language.service';
-
 export class BaseLookupModel {
   id?: number;
   nameAr?: string;
   nameEn?: string;
-  private languageService?: LanguageService;
-
-  constructor() {
-    this.languageService = FactoryService.getService('LanguageService');
-  }
-
-  getName(): string {
-    return this.languageService?.getCurrentLanguage() == LANGUAGE_ENUM.ENGLISH
-      ? this.nameEn || ''
-      : this.nameAr || '';
-  }
 }

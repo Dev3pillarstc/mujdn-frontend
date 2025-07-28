292d472f82bdb9da4a7ce80ca552327c04daf848
Ahmed Elgohary <ahmed.elgohary@3pillars-eg.com>
2025-07-28 10:48:26 +0300

    (reopen) MUJ-263 : Incorrect Validations and Required Fields For Adding New Department Fields

--- src/models/features/lookups/department/department.ts
+++ src/models/features/lookups/department/department.ts
@@ -65,7 +65,8 @@ export class Department extends BaseCrudModel<Department, DepartmentService> {
         [
           Validators.required,
           Validators.minLength(CustomValidators.defaultLengths.MIN_LENGTH),
-          CustomValidators.pattern('ENG_AR_ONLY'),
+          Validators.maxLength(CustomValidators.defaultLengths.ENGLISH_NAME_MAX),
+          CustomValidators.pattern('ENG_AR_NUM_ONLY'),
         ],
       ],
       phoneNumber: [

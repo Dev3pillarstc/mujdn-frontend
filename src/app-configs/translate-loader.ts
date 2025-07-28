c9774e390ba3183707ada98ddc0a7c5ad6700ef4
Mahmoud Soliman <mahmoud.soliman@3pillars-eg.com>
2025-07-28 16:17:51 +0300

    fix translation files prefix on server

--- src/app-configs/translate-loader.ts
+++ src/app-configs/translate-loader.ts
@@ -5,9 +5,9 @@ import { ConfigService } from '@/services/config.service';
 
 export function HttpLoaderFactory(http: HttpClient) {
   const configService = inject(ConfigService);
-  return new TranslateHttpLoader(
-    http,
-    'assets/i18n/',
-    `.json?v=${configService.CONFIG.DEPLOY_VERSION}`
-  );
+
+  const prefix =
+    configService.CONFIG.BASE_ENVIRONMENT === 'DEV' ? 'assets/i18n/' : 'resources/i18n/';
+
+  return new TranslateHttpLoader(http, prefix, `.json?v=${configService.CONFIG.DEPLOY_VERSION}`);
 }

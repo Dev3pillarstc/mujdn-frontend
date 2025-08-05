f777d72ee979548cb49d9b4b873621c10947c12e
Ahmed Elgohary <ahmed.elgohary@3pillars-eg.com>
2025-08-05 19:24:05 +0300

    finishing working days settings

---
+++ src/models/features/setting/work-days-setting.ts
@@ -0,0 +1,38 @@
+import { BaseCrudModel } from "@/abstracts/base-crud-model";
+import { WorkDaysSettingInterceptor } from "@/model-interceptors/setting/work-days-setting.interceptor";
+import { NotificationSettingService } from "@/services/features/setting/notification-setting.service";
+import { WorkDaysSettingService } from "@/services/features/setting/work-days-setting.service";
+import { InterceptModel } from "cast-response";
+
+const { send, receive } = new WorkDaysSettingInterceptor();
+
+@InterceptModel({ send, receive })
+export class WorkDaysSetting extends BaseCrudModel<
+    WorkDaysSetting,
+    WorkDaysSettingService
+> {
+    override $$__service_name__$$: string = 'WorkDaysSettingService';
+    sunday: boolean = true;
+    monday: boolean = true;
+    tuesday: boolean = true;
+    wednesday: boolean = true;
+    thursday: boolean = true;
+    friday: boolean = true;
+    saturday: boolean = true;
+    declare concurrencyUpdateVersion?: Uint8Array;
+
+    buildForm() {
+        const { sunday, monday, tuesday, wednesday, thursday, friday, saturday } = this;
+        const form = {
+            sunday: [sunday, []],
+            monday: [monday, []],
+            tuesday: [tuesday, []],
+            wednesday: [wednesday, []],
+            thursday: [thursday, []],
+            friday: [friday, []],
+            saturday: [saturday, []],
+        };
+
+        return form;
+    }
+}
---
+++ src/services/features/setting/work-days-setting.service.ts
@@ -0,0 +1,55 @@
+import { BaseCrudService } from "@/abstracts/base-crud-service";
+import { WorkDaysSetting } from "@/models/features/setting/work-days-setting";
+import { ResponseData } from "@/models/shared/response/response-data";
+import { Injectable } from "@angular/core";
+import { CastResponse, CastResponseContainer, HasInterception, InterceptParam } from "cast-response";
+import { Observable, of, switchMap } from "rxjs";
+
+@CastResponseContainer({
+    $default: {
+        model: () => WorkDaysSetting,
+    },
+    $get: {
+        model: () => WorkDaysSetting,
+        unwrap: 'data',
+        shape: { data: () => WorkDaysSetting },
+    },
+})
+@Injectable({
+    providedIn: 'root',
+})
+export class WorkDaysSettingService extends BaseCrudService<WorkDaysSetting> {
+    serviceName = 'WorkDaysSettingService';
+
+    override getUrlSegment(): string {
+        return this.urlService.URLS.NOTIFICATION_SETTINGS;
+    }
+
+    @CastResponse(undefined, { fallback: '$get' })
+    @HasInterception
+    getWorkDays(): Observable<WorkDaysSetting> {
+        return this.http
+            .get<ResponseData<WorkDaysSetting>>(this.getUrlSegment() + '/' + 'GetWorkDays', {
+                withCredentials: true,
+            })
+            .pipe(
+                switchMap((response: ResponseData<WorkDaysSetting>) => {
+                    return of(response.data);
+                })
+            );
+    }
+
+    @CastResponse(undefined, { fallback: '$get' })
+    @HasInterception
+    updateWorkDays(@InterceptParam() data: WorkDaysSetting): Observable<WorkDaysSetting> {
+        return this.http
+            .put<ResponseData<WorkDaysSetting>>(this.getUrlSegment() + '/' + 'UpdateWorkDays', data, {
+                withCredentials: true,
+            })
+            .pipe(
+                switchMap((response: ResponseData<WorkDaysSetting>) => {
+                    return of(response.data);
+                })
+            );
+    }
+}

b6183afd8675f576e908a78ea6431a9721e3ca03
Mahmoud Soliman <mahmoud.soliman@3pillars-eg.com>
2025-08-06 09:35:35 +0300

    format

--- src/resolvers/setting/notification-setting.resolver.ts
+++ src/resolvers/setting/notification-setting.resolver.ts
@@ -6,7 +6,9 @@ import { catchError, forkJoin, of, tap } from 'rxjs';
 import { WorkDaysSettingService } from '@/services/features/setting/work-days-setting.service';
 import { WorkDaysSetting } from '@/models/features/setting/work-days-setting';
 
-export const notificationSettingResolver: ResolveFn<{ notificationSetting: NotificationSetting, workDays: WorkDaysSetting } | null> = () => {
+export const notificationSettingResolver: ResolveFn<
+  { notificationSetting: NotificationSetting; workDays: WorkDaysSetting } | null
+> = () => {
   const notificationSettingService = inject(NotificationSettingService);
   const workDaysSettingService = inject(WorkDaysSettingService);
 

81b87e1136fc9ce050a73986a794a5a78cafcb63
Mahmoud Soliman <mahmoud.soliman@3pillars-eg.com>
2025-08-24 18:30:00 +0300

    format

--- src/views/features/outside-mission/outside-mission-list/outside-mission-list.component.ts
+++ src/views/features/outside-mission/outside-mission-list/outside-mission-list.component.ts
@@ -4,26 +4,18 @@ import { Breadcrumb } from 'primeng/breadcrumb';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { PaginatorModule, PaginatorState } from 'primeng/paginator';
+import { PaginatorModule } from 'primeng/paginator';
 import { InputTextModule } from 'primeng/inputtext';
 import { DatePickerModule } from 'primeng/datepicker';
 import { FormsModule } from '@angular/forms';
-import { AddNewMissionPopupComponent } from '../popups/add-new-mission-popup/add-new-mission-popup.component';
 import { MatDialog } from '@angular/material/dialog';
-import { ViewMissionDataPopupComponent } from '../popups/view-mission-data-popup/view-mission-data-popup.component';
 import { TranslatePipe } from '@ngx-translate/core';
-import { AssignEmployeesComponent } from '../popups/assign-employees/assign-employees.component';
 import { TabsModule } from 'primeng/tabs';
-import { Select } from 'primeng/select';
-import { SplitButton } from 'primeng/splitbutton';
 import { MyMissionsTabComponent } from '../my-missions-tab/my-missions-tab.component';
 import { AssignAndViewMissionsTabComponent } from '../assign-and-view-missions-tab/assign-and-view-missions-tab.component';
 import { PaginatedList } from '@/models/shared/response/paginated-list';
 import { WorkMission } from '@/models/features/business/work-mission';
-import { ListResponseData } from '@/models/shared/response/list-response-data';
 import { BaseLookupModel } from '@/models/features/lookups/base-lookup-model';
-import { UserProfileDataWithNationalId } from '@/models/features/business/user-profile-data-with-national-id';
-import { PaginatedListResponseData } from '@/models/shared/response/paginated-list-response-data';
 import { AuthService } from '@/services/auth/auth.service';
 
 @Component({
@@ -39,8 +31,6 @@ import { AuthService } from '@/services/auth/auth.service';
     TabsModule,
     FormsModule,
     TranslatePipe,
-    Select,
-    SplitButton,
     MyMissionsTabComponent,
     AssignAndViewMissionsTabComponent,
   ],

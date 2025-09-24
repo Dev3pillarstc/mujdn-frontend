5697f5fff5e38b99a387e87745ebcced0a8d4aec
Youssef <youssef.hassan@3pillars-eg.com>
2025-09-24 14:55:04 +0300

    Adding Permissions to attendence report

--- src/views/features/reports/attendance-report/all-attendance-report-list/all-attendance-report-list.component.ts
+++ src/views/features/reports/attendance-report/all-attendance-report-list/all-attendance-report-list.component.ts
@@ -176,4 +176,17 @@ export class AllAttendanceReportListComponent extends BaseListComponent<
   isCurrentLanguageEnglish() {
     return this.languageService.getCurrentLanguage() == LANGUAGE_ENUM.ENGLISH;
   }
+
+  getPermissionLabel(attendance: any): string {
+    if (attendance.attendancePermissionId && attendance.leavePermissionId) {
+      return 'ATTENDANCE_REPORT_PAGE.PRESENCE_LEAVE';
+    }
+    if (attendance.leavePermissionId && !attendance.attendancePermissionId) {
+      return 'ATTENDANCE_REPORT_PAGE.LEAVE';
+    }
+    if (attendance.attendancePermissionId && !attendance.leavePermissionId) {
+      return 'ATTENDANCE_REPORT_PAGE.PRESENCE';
+    }
+    return '';
+  }
 }

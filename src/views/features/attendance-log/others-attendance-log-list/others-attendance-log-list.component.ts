e75aee780b8bf7404ec835b6178df1db2a823e2a
kareem-shehab <Kareem.Shehab@3pillarstc.com>
2025-10-23 12:43:48 +0300

    finish modals

--- src/views/features/attendance-log/others-attendance-log-list/others-attendance-log-list.component.ts
+++ src/views/features/attendance-log/others-attendance-log-list/others-attendance-log-list.component.ts
@@ -359,8 +359,8 @@ export default class OthersAttendanceLogListComponent
     dialogConfig.data = {
       model: model,
     };
-    dialogConfig.width = this.dialogSize2.width;
-    dialogConfig.maxWidth = this.dialogSize2.maxWidth;
+    dialogConfig.width = this.dialogSize.width;
+    dialogConfig.maxWidth = this.dialogSize.maxWidth;
     const dialogRef = this.matDialog.open(ImportLogPopupComponent as any, dialogConfig);
 
     return dialogRef.afterClosed().subscribe((result: DIALOG_ENUM) => {

adef5d1dcb7be4dd172ff375b0e50ce0e46a9abf
Mahmoud Soliman <mahmoud.soliman@3pillars-eg.com>
2026-05-05 17:36:04 +0300

    render shift names in the list

---
+++ src/models/features/lookups/work-shifts/rotation-group.ts
@@ -0,0 +1,10 @@
+import { ShiftDetails } from '@/models/features/lookups/work-shifts/shift-details';
+
+export class RotationGroup {
+  declare id: number;
+  declare groupName: string;
+  declare shiftDetails: ShiftDetails;
+  declare periodOrder: number;
+  declare label: string;
+  memberIds: number[] = [];
+}
